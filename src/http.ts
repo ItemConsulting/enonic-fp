b90bfab209c3d35a5bcd0e44ccc1efa96987caa2
Tom Arild Jakobsen <tajakobsen@gmail.com>
2019-08-30 08:11:38 +0200

    Add support for enonic http lib
    Closes #2

---
+++ src/http.ts
@@ -0,0 +1,49 @@
+import { Either, tryCatch } from "fp-ts/lib/Either";
+import { Error } from "./common";
+const httpClient = __non_webpack_require__('/lib/http-client');
+
+interface HttpRequestParamsProxy {
+  host?: string
+  port?: number
+  user?: string
+  password?: string
+}
+
+interface HttpRequestParamsAuth {
+  user?: string
+  password?: string
+}
+
+export interface HttpRequestParams {
+  url: string
+  method?: string
+  params?: object
+  headers?: object
+  connectionTimeout?: number
+  readTimeout?: number
+  body?: string | any
+  contentType?: string
+  multipart?: Array<object>
+  auth?: HttpRequestParamsAuth
+  proxy?: HttpRequestParamsProxy,
+  followRedirects?: boolean
+}
+
+export interface HttpResponse {
+  status: number
+  message: string
+  headers: object
+  contentType: string
+  body: string | null
+  bodyStream: any
+}
+
+export function request(params: HttpRequestParams) : Either<Error, HttpResponse> {
+  return tryCatch(
+    () => httpClient.request(params),
+    (e) => ({
+      errorKey: "BadGatewayError",
+      cause: String(e)
+    })
+  )
+}
