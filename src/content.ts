80607e15c82626bd66d15989385d9bed8fa5ee25
Tom Arild Jakobsen <tajakobsen@gmail.com>
2019-09-03 14:24:09 +0200

    Add suport for all types in Content lib
    Closes #4

--- src/content.ts
+++ src/content.ts
@@ -5,7 +5,7 @@ const content = __non_webpack_require__('/lib/xp/content');
 
 export interface Content<T> {
   _id: string,
-  name: string,
+  _name: string,
   _path: string,
   creator: string,
   modifier: string,
@@ -21,9 +21,18 @@ export interface Content<T> {
   data: T,
   x: { [key:string]: string },
   page: any,
-  attachments: object,
+  attachments: Attachments,
   publish: any
 }
+
+export interface Attachment {
+  name: string,
+  label?: string,
+  size: number,
+  mimeType: string
+}
+
+export type Attachments = { [key: string]: Attachment }
 
 export interface QueryContentParams {
   start?: number,
@@ -65,7 +74,7 @@ export interface CreateContentParams<T> {
 
 export interface ModifyContentParams<T> {
   key: string,
-  editor: (c?: Content<T>) => Content<T>,
+  editor: (c: Content<T>) => Content<T>,
   requireValid?: boolean
 }
 
@@ -83,11 +92,116 @@ export interface ScheduleParams {
   to: string
 }
 
-
 export interface PublishResponse {
   pushedContents: Array<string>,
   deletedContents: Array<string>,
   failedContents: Array<string>
+}
+
+export interface UnpublishContentParams {
+  keys: string[]
+}
+
+export interface GetChildrenParams {
+  key: string
+  start?: number
+  count?: number
+  sort?: string
+}
+
+export interface MoveParams {
+  source: string
+  target: string
+}
+
+export interface GetSiteParams {
+  key: string
+}
+
+export interface Site<T> {
+  _id: string,
+  _name: string,
+  _path: string,
+  type: string,
+  hasChildren: Boolean,
+  valid: Boolean,
+  data: {
+    siteConfig: SiteConfig<T>
+  },
+  x: { [key:string]: string },
+  page: any,
+  attachments: object,
+  publish: any
+}
+
+export interface SiteConfig<T> {
+  applicationKey: string,
+  config: T,
+}
+
+export interface GetSiteConfigParams {
+  key: string,
+  applicationKey: string
+}
+
+export interface AttachmentStreamParams {
+  key: string
+  name: string
+}
+
+export interface RemoveAttachmentParams {
+  key: string
+  name: string | string[]
+}
+
+export interface CreateMediaParams {
+  name?: string
+  parentPath?: string
+  mimeType?: string
+  focalX?: number
+  focalY?: number
+  data: any // stream
+}
+
+export interface GetPermissionsParams {
+  key: string
+}
+
+export interface GetPermissionsResult {
+  inheritsPermissions: boolean
+  permissions: PermissionsParams[]
+}
+
+export interface PermissionsParams {
+  principal: string
+  allow: string[]
+  deny: string[]
+}
+
+export interface SetPermissionsParams {
+  key: string,
+  inheritPermissions: boolean
+  overwriteChildPermissions: boolean
+  permissions: PermissionsParams[]
+}
+
+export interface IconType {
+  data?: any,
+  mimeType?: string
+  modifiedTime?: string
+}
+
+export interface ContentType {
+  "name": string,
+  "displayName": string
+  "description": string
+  "superType": string
+  "abstract": boolean
+  "final": boolean
+  "allowChildContent": boolean
+  "displayNameExpression": string
+  "icon": IconType[]
+  "form": any[]
 }
 
 export function get<T>(params: GetContentParams) :Either<Error, Content<T>> {
@@ -139,4 +253,102 @@ export function publish(params: PublishContentParams) :Either<Error, PublishResp
     () => content.publish(params),
     (e) => ({ errorKey: "PublishError", cause: String(e) })
   )
+}
+
+export function unpublish(params: UnpublishContentParams) :Either<Error, string[]> {
+  return tryCatch<Error, string[]>(
+    () => content.unpublish(params),
+    (e) => ({ errorKey: "PublishError", cause: String(e) })
+  )
+}
+
+export function getChildren<T>(params: GetChildrenParams) :Either<Error, QueryResponse<T>> {
+  return tryCatch<Error, QueryResponse<T>>(
+    () => content.getChildren(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function move<T>(params: MoveParams) :Either<Error, Content<T>> {
+  return tryCatch<Error, Content<T>>(
+    () => content.move(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function getSite<T>(params: GetSiteParams) :Either<Error, Site<T>> {
+  return tryCatch<Error, Site<T>>(
+    () => content.getSite(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function getSiteConfig<T>(params: GetSiteConfigParams) :Either<Error, T> {
+  return tryCatch<Error, T>(
+    () => content.getSiteConfig(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function createMedia<T>(params: CreateMediaParams) :Either<Error, Content<T>> {
+  return tryCatch<Error, Content<T>>(
+    () => content.createMedia(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function getAttachments(key: string) :Either<Error, Attachments> {
+  return pipe(
+    tryCatch<Error, Attachments>(
+      () => content.getAttachments(key),
+      (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+    ),
+    chain(fromNullable<Error>({ errorKey: "NotFoundError" }))
+  );
+}
+
+// The return type is Java: com.google.common.io.ByteSource
+export function getAttachmentStream(params: AttachmentStreamParams) : Either<Error, any> {
+  return pipe(
+    tryCatch<Error, any>(
+      () => content.getAttachmentStream(params),
+      (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+    ),
+    chain(fromNullable<Error>({ errorKey: "NotFoundError" }))
+  );
+}
+
+export function removeAttachment(params: RemoveAttachmentParams) : Either<Error, void> {
+  return tryCatch<Error, void>(
+    () => content.removeAttachment(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  );
+}
+
+export function getPermissions(params: GetPermissionsParams) : Either<Error, GetPermissionsResult> {
+  return tryCatch<Error, GetPermissionsResult>(
+    () => content.getPermissions(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  );
+}
+
+export function setPermissions(params: SetPermissionsParams) : Either<Error, GetPermissionsResult> {
+  return tryCatch<Error, GetPermissionsResult>(
+    () => content.setPermissions(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  );
+}
+
+export function getType(name: string) : Either<Error, ContentType> {
+  return tryCatch<Error, ContentType>(
+    () => content.getType(name),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  );
+}
+
+export function getTypes() : Either<Error, ContentType[]> {
+  return tryCatch<Error, ContentType[]>(
+    () => content.getTypes(),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  );
 }
