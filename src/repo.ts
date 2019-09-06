db3e16c9d07ca65826b9af0c07f1a8c95e3f7447
Tom Arild Jakobsen <tajakobsen@gmail.com>
2019-09-06 14:07:02 +0200

    Add support for repo library
    Closes: #11

---
+++ src/repo.ts
@@ -0,0 +1,105 @@
+import {chain, Either, fromNullable, tryCatch} from "fp-ts/lib/Either";
+import {Error} from "./common";
+import {PermissionsParams} from "./content";
+import {pipe} from "fp-ts/lib/pipeable";
+const repo = __non_webpack_require__('/lib/xp/repo');
+
+export interface IndexDefinition {
+  settings: any
+  mapping: any
+}
+
+export interface CreateRepoParams {
+  id: string
+  rootPermissions?: Array<PermissionsParams>
+  settings?: {
+    definitions?: {
+      search?: IndexDefinition
+      version?: IndexDefinition
+      branch?: IndexDefinition
+    }
+  }
+}
+
+export interface CreateBranchParams {
+  branchId: string
+  repoId: string
+}
+
+export interface RepositoryConfig {
+  id: string,
+  branches: Array<string>
+  settings: any
+}
+
+export interface RefreshParams {
+  mode?: string
+  repo?: string
+  branch?: string
+}
+
+export interface DeleteBranchParams {
+  branchId: string
+  repoId: string
+}
+
+export function create(params: CreateRepoParams): Either<Error, RepositoryConfig> {
+  return tryCatch<Error, RepositoryConfig>(
+    () => repo.create(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function createBranch(params: CreateBranchParams): Either<Error, RepositoryConfig> {
+  return tryCatch<Error, RepositoryConfig>(
+    () => repo.createBranch(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function get(id: string): Either<Error, RepositoryConfig> {
+  return pipe(
+    tryCatch<Error, RepositoryConfig>(
+      () => repo.get(id),
+      (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+    ),
+    chain(fromNullable<Error>({ errorKey: "NotFoundError" }))
+  );
+}
+
+export function list(): Either<Error, Array<RepositoryConfig>> {
+  return tryCatch<Error, Array<RepositoryConfig>>(
+    () => repo.list(),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function remove(id: string): Either<Error, boolean> {
+  return tryCatch<Error, boolean>(
+    () => repo.delete(id),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
+
+export function deleteBranch(params: DeleteBranchParams): Either<Error, any> { // Figure out the shape here for any
+  return pipe(
+    tryCatch<Error, any>(
+      () => repo.deleteBranch(params),
+      (e: any) => {
+        return {
+          errorKey: (e.code === 'branchNotFound')
+            ? "NotFoundError"
+            : "InternalServerError",
+          cause: String(e)
+        }
+      }
+    )
+  );
+}
+
+export function refresh(params: RefreshParams): Either<Error, Array<RepositoryConfig>> {
+  return tryCatch<Error, Array<RepositoryConfig>>(
+    () => repo.refresh(params),
+    (e) => ({ errorKey: "InternalServerError", cause: String(e) })
+  )
+}
