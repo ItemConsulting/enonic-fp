9b736b4b408144d7e685ebf298fc4c77a312fbd0
Tom Arild Jakobsen <tajakobsen@gmail.com>
2019-12-19 11:12:29 +0100

    Add MenuLib support

---
+++ src/menu.ts
@@ -0,0 +1,25 @@
+import {BreadcrumbMenu, GetBreadcrumbMenuParams, MenuItem, MenuLibrary} from "enonic-types/lib/menu";
+import {Content} from "enonic-types/lib/content";
+import {catchEnonicError} from "./utils";
+import {IOEither} from "fp-ts/lib/IOEither";
+import {EnonicError} from "./errors";
+
+const menuLib: MenuLibrary = __non_webpack_require__('/lib/menu');
+
+export function getBreadcrumbMenu(params: GetBreadcrumbMenuParams): IOEither<EnonicError, BreadcrumbMenu> {
+  return catchEnonicError(
+    () => menuLib.getBreadcrumbMenu(params)
+  );
+}
+
+export function getMenuTree(levels: number): IOEither<EnonicError, ReadonlyArray<MenuItem>> {
+  return catchEnonicError(
+    () => menuLib.getMenuTree(levels)
+  );
+}
+
+export function getSubMenus(parentContent: Content<any>, levels: number): IOEither<EnonicError, ReadonlyArray<MenuItem>> {
+  return catchEnonicError(
+    () => menuLib.getSubMenus(parentContent, levels)
+  );
+}
