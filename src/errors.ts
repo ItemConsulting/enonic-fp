b60b719ced6e01fa2a7004dd2a53a77586f01acb
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-02-25 10:21:26 +0100

    Add alternative for "TooManyRequestsError"

--- src/errors.ts
+++ src/errors.ts
@@ -8,6 +8,7 @@ export type GeneralEnonicErrorKey =
   | "NotFoundError"
   | "MethodNotAllowedError"
   | "InternalServerError"
+  | "TooManyRequestsError"
   | "BadGatewayError"
   | "PublishError";
 
